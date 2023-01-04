87f724d7c32c54fc01a8c2ba9a8c4d282f25d5d4
Samuel Njoroge <njorogekiragu8@gmail.com>
2023-01-04 16:56:15 +0300

    Add files via upload

---
+++ match1.py
@@ -0,0 +1,7 @@
+# A sequence of characters that define a search pattern .
+# Can be used in a string-searching algorithm to find and replace.
+
+# A  pattern that takes HTML code and removes comments
+import re
+    
+print(re.match('meow', 'hormon'))
